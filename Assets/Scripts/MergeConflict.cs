a183419bce586077bc9837f6cb256892dedc142e
kerri22 <105732840+kerri22@users.noreply.github.com>
2025-10-29 21:54:33 +0000

    same script name different branches

---
+++ Assets/Scripts/MergeConflict.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MergeConflict : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Debug.Log("This is a merge conflict script on main");
+    }
+}
