93b4e76692bb14103fd4484b682b3dfc159af01d
Kerri Thornton <64016374+kerrisarahthorntonGSA@users.noreply.github.com>
2026-02-02 11:01:49 +0000

    Made the Debug Script for Develop

---
+++ Assets/Scripts/DebugScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DebugScript : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("This is the Develop Script");
+    }
+}
