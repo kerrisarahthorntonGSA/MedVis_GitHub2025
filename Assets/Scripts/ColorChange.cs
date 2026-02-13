6336262bc653260fb80d471c0953e278b40e934c
Kerri Thornton <64016374+kerrisarahthorntonGSA@users.noreply.github.com>
2026-02-13 11:10:04 +0000

    added color change script

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    // This script will look at Mouse Inputs
+
+    public GameObject objToEnable; //this is a variable
+    public Color clickedColorChange; // this is a variable
+    Color dodgerBlue = new Color(0.1176471f, 0.5647059f, 1f, 1f);
+    
+
+    Renderer rend; //Makes an instance of the renderer component of this script to access it
+
+
+    void Start()
+    {
+       rend = GetComponent<Renderer>(); 
+       //assigns the instance of the renderer component object we made in script memory to be the same renderer component on the object this script is attached to
+    }
+
+    void Update()
+    {
+        KerriCustomMouseMethod();
+    }
+
+    void OnMouseDown() //this is a method that is part of Unity's Libraries/Monobehaviour
+    {
+        Debug.Log("LeftMouse Button Clicked");
+       objToEnable.SetActive(true);
+       rend.material.color = dodgerBlue; //;//changes the color of the material to green using the variable we assigned earlier
+
+    }
+
+    void KerriCustomMouseMethod() //this is a custom method I am creating...but how can i get this to run?
+    {
+        if(Input.GetAxis("Mouse X")<0)//we are using operaters <==> to tell the system the values
+        {
+            Debug.Log("Mouse is moving to the left");
+        }
+        if(Input.GetAxis("Mouse X")==0) // == assigns a value
+        {
+            Debug.Log("Mouse not moving");
+        }
+        if(Input.GetAxis("Mouse X")>0)
+        {
+            Debug.Log("Mouse is moving to the right");
+        }
+    }
+}
